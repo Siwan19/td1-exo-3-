9289785f9308390f24cc4b14eb99951acad98e8c
Siwan19 <68042209+Siwan19@users.noreply.github.com>
2020-07-09 14:58:49 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    if (nb < 10) {
+        nb += 1
+        basic.showNumber(nb)
+    }
+})
+let nb = 0
+nb = 1
+basic.showNumber(nb)
+basic.forever(function () {
+	
+})
